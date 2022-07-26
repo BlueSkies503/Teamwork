b4e4a02ff6a6e1b127ba13e0512f260ceabf3800
dodecaphonix <dodecaphonix@gmail.com>
2022-07-25 23:00:56 -0700

    Use templates (#2)

--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+)
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template('index.html', who="World")
 
 
 if __name__ == "__main__":
