5e6d08ec3f2a485942566635d61a3b03029a138f
Hunter Matthews <huntermatthews@Hunters-MacBook-Pro.local>
2022-07-27 13:41:30 -0700

    Change route name and update README.md

--- randommm.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-
-num = randint(1, 10)
-print(num)
--- routes.py
+++ routes.py
@@ -2,6 +2,11 @@ from flask import Flask
 from random import randint
 
 
+# lower and upper bounds for random number choices
+LOWER = 1
+UPPER = 20
+
+
 app = Flask(__name__)
 
 
@@ -10,9 +15,9 @@ def hello_world():
     return "<h1>Hello, World!</h1>"
 
 
-@app.route("/foo")
+@app.route("/rand_num")
 def rand_element():
-    return f"<h1>{randint(1, 20)}</h1>"
+    return f"<h1>{randint(LOWER, UPPER)}</h1>"
 
 
 if __name__ == "__main__":
