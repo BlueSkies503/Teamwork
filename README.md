1f4e8642ecad5a7a2f40c0f6e294371e792cbd33
Hunter Matthews <35434608+BlueSkies503@users.noreply.github.com>
2022-07-28 14:14:55 -0700

    Add random number route (#3)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Teamwork
+
+## Goal
+Our goal is to randomly generate an algorithm to scramble a Rubik's Cube, example shown below.
+
+Scramble:  U  D2  B'  F'  D  F'  D  U'  B2  R'  B'  R  D2  F2  U'  L2  U  L  R'  D2  F2  D  B2  D2  L
--- routes.py
+++ routes.py
@@ -2,6 +2,13 @@ from flask import (
     Flask,
     render_template,
 )
+from random import randint
+
+
+# lower and upper bounds for random number choices
+LOWER = 1
+UPPER = 20
+
 
 app = Flask(__name__)
 
@@ -9,6 +16,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html', who="World")
+
+
+@app.route("/rand_num")
+def rand_num():
+    return render_template("rand_num.html", num=randint(LOWER, UPPER))
 
 
 if __name__ == "__main__":
