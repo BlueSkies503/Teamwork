6ca85a8b048c580093400d6a4262e4c899b4d564
dodecaphonix <dodecaphonix@gmail.com>
2022-07-28 19:28:38 -0700

    Make project pass lint

--- routes.py
+++ routes.py
@@ -1,8 +1,14 @@
+"""
+This file defines the routes for a flask application.
+If run directly, it will spin up a development server.
+"""
+
+from random import randint
+
 from flask import (
     Flask,
     render_template,
 )
-from random import randint
 
 
 # lower and upper bounds for random number choices
@@ -15,11 +21,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    This route displays an HTML frontend Hello World
+
+    :return: Rendered HTML template
+    """
     return render_template('index.html', who="World")
 
 
 @app.route("/rand_num")
 def rand_num():
+    """
+    This route displays an HTML frontend random number
+
+    :return: Rendered HTML template
+    """
     return render_template("rand_num.html", num=randint(LOWER, UPPER))
 
 
