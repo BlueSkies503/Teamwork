900671df4bbee388ba395e7b7ae22e0317255860
Hunter Matthews <huntermatthews@Hunters-MacBook-Pro.local>
2022-08-06 19:36:39 -0700

    Implement scramble functionality

--- cube.py
+++ cube.py
@@ -10,28 +10,38 @@ import random
 K = 25
 
 
-""" A list of possible modifications on a move. To be appended to a
-face value like so: 'U2'.
-"""
-MODS = ["2", "'"]
-
-
 class Cube:
     """
     Class attribute: A list of faces on the cube,
     U : Up, D : Down (bottom face), and so on.
     """
-    FACES = ['U', 'D', 'F', 'R', 'B', 'L']
-
     def __init__(self, order):
         self.order = order
 
     # Instance method randomly chooses from FACES, K times
+    @staticmethod
+    def generate_scramble():
+        scramble = [RandomScrambleStep()]
+        while len(scramble) < K:
+            step = RandomScrambleStep()
+            if scramble[-1].face != step.face:
+                scramble.append(step)
+        return scramble
+
+
+class RandomScrambleStep:
+    """
+    Is a single turn of the cube.
+    """
+    MODS = ["2", "'", ""]  # A list of possible modifications on a turn
+    FACES = ['U', 'D', 'F', 'R', 'B', 'L']
+
+    def __init__(self):
+        self.face = random.choice(RandomScrambleStep.FACES)
+        self.mod = random.choice(RandomScrambleStep.MODS)
+
     def __str__(self):
-        scramble_alg = random.choices(Cube.FACES, k = K)
-        return f'Your scramble: {scramble_alg}'
+        return f'{self.face}{self.mod}'
 
-
-three_by_three = Cube(3)
-
-print(three_by_three)
+    def __repr__(self):
+        return self.__str__()
